2e2311c31fad198468a484bb280e015c5c31f7ef
Gabor Greif <ggreif@gmail.com>
2012-09-25 12:31:19 +0200

    add a criterion for loading unconstrained types; this is the completely (type-)unsafe way

---
+++ System/Plugins/Criteria/UnsafeCriterion.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE KindSignatures, ConstraintKinds, TypeFamilies,
+             MultiParamTypeClasses, FlexibleInstances #-}
+
+module System.Plugins.Criteria.UnsafeCriterion where
+
+import System.Plugins.Criteria.LoadCriterion
+import System.Plugins.DynamicLoader
+
+instance LoadCriterion () t where
+  data Criterion () t = UnsafeCriterion
+  type Effective () t = t
+  loadQualified UnsafeCriterion = loadQualifiedFunction
+
