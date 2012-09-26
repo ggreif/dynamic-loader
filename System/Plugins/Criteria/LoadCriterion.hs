cba8b6c13fef8dd12b3db339d033990145a3bd11
Gabor Greif <ggreif@gmail.com>
2012-09-26 13:24:18 +0200

    export fundamental functionality: addDynamicLibrary resolveSymbols

--- System/Plugins/Criteria/LoadCriterion.hs
+++ System/Plugins/Criteria/LoadCriterion.hs
@@ -6,9 +6,14 @@ module System.Plugins.Criteria.LoadCriterion (LoadCriterion(..)) where
 -- The 'Constraint' kind is defined in 'GHC.Exts'
 
 import GHC.Exts
+import System.Plugins.DynamicLoader
 
 class LoadCriterion (c :: Constraint) t where
   data Criterion c t
   type Effective c t :: *
+  addDynamicLibrary :: Criterion c t -> String -> IO ()
+  addDynamicLibrary _ = addDLL
+  resolveSymbols :: Criterion c t -> IO ()
+  resolveSymbols _ = resolveFunctions
   loadQualified :: c => Criterion c t -> String -> IO (Effective c t)
 
