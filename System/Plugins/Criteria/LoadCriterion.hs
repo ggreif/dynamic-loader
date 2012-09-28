1bd56424054eaaf8692465201032c66e5578598d
Gabor Greif <ggreif@gmail.com>
2012-09-28 10:35:18 +0200

    reduce clutter, safe criteria are in LoadCriterion.hs

--- System/Plugins/Criteria/LoadCriterion.hs
+++ System/Plugins/Criteria/LoadCriterion.hs
@@ -7,6 +7,7 @@ module System.Plugins.Criteria.LoadCriterion (LoadCriterion(..)) where
 
 import GHC.Exts
 import System.Plugins.DynamicLoader
+import Data.Dynamic
 
 class LoadCriterion (c :: Constraint) t where
   data Criterion c t
@@ -17,3 +18,17 @@ class LoadCriterion (c :: Constraint) t where
   resolveSymbols _ = resolveFunctions
   loadQualified :: c => Criterion c t -> String -> IO (Effective c t)
 
+
+-- Safe criteria follow
+
+instance LoadCriterion (Typeable t) t where
+  data Criterion (Typeable t) t = DynamicCriterion
+  type Effective (Typeable t) t = Maybe t
+  loadQualified DynamicCriterion name = loadQualifiedDynFunction (adornSymbol name)
+    where adornSymbol n = n ++ "Dyn"
+
+
+loadQualifiedDynFunction :: Typeable t => String -> IO (Maybe t)
+loadQualifiedDynFunction name = fmap fromDynamic dyn
+  where dyn :: IO Dynamic
+        dyn = loadQualifiedFunction name
